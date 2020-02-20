cc11f67603739ed84b518083fc39eec52a3bd35d
Nadan Gergeo <nadan.gergeo@gmail.com>
2020-02-20 07:48:38 +0100

    Use sass instead of using calc and css vars

--- src/Blimp/index.js
+++ src/Blimp/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './index.css';
+import './index.scss';
 
 const BlimpSVG = (props) => (
   <svg className="blimp__svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 100 100" xmlSpace="preserve">
